64b03f883ef28fbcd12f22c46257164397d2d80c
anishazahan <anishazahan13@gmail.com>
2023-05-28 11:35:04 +0600

    createBrowserRouter implement

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl text-red-600 font-bold underline">
       Hello world!
     </h1>
     </>
