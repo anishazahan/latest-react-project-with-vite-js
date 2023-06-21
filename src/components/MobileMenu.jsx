1f22c923d564f569004d17cf3c0b4f7f854d113d
anishazahan <anishazahan13@gmail.com>
2023-06-21 17:11:41 +0600

    responsive menubar create

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const MobileMenu = () => {
+const MobileMenu = (setIsMenuOpen) => {
   return (
     <>
      <nav className="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
@@ -8,7 +8,7 @@ const MobileMenu = () => {
 				<a className="mr-auto text-3xl font-bold leading-none" href="#">
 					
 				</a>
-				<button className="navbar-close">
+				<button  onClick={() => setIsMenuOpen(false)} className="navbar-close">
 					<svg className="h-6 w-6 text-gray-400 cursor-pointer hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 						<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
 					</svg>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MobileMenu from './MobileMenu'
 import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
 
-
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   return (
     <>
@@ -18,15 +18,15 @@ const Navbar = () => {
 		
 		<ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:items-center lg:w-auto lg:space-x-6">
 
-			<li><Link to='/' className="text-sm text-gray-400 hover:text-gray-500" href="#">Home</Link></li>
+		<li><NavLink to='/'  className={({ isActive }) => (isActive ? 'text-blue-500 font-bold ' : 'text-sm text-gray-400 hover:text-gray-500') } >Home</NavLink></li>
 			
-			<li><NavLink to='/about'  className={({ isActive }) => (isActive ? 'text-blue-500 font-bold' : 'text-sm text-gray-400 hover:text-gray-500') } >About Us</NavLink></li>
+			<li><NavLink to='/about'  className={({ isActive }) => (isActive ? 'text-blue-500 font-bold ' : 'text-sm text-gray-400 hover:text-gray-500') } >About Us</NavLink></li>
 			
-			<li><Link to='/services' className="text-sm text-gray-400 hover:text-gray-500" href="#">Service</Link></li>
+			<li><NavLink to='/services'  className={({ isActive }) => (isActive ? 'text-blue-500 font-bold ' : 'text-sm text-gray-400 hover:text-gray-500') } >Services</NavLink></li>
 			
-			<li><Link to='/books' className="text-sm text-gray-400 hover:text-gray-500" href="#">Books</Link></li>
+			<li><NavLink to='/books'  className={({ isActive }) => (isActive ? 'text-blue-500 font-bold ' : 'text-sm text-gray-400 hover:text-gray-500') } >Books</NavLink></li>
 		
-			<li><Link to='/' className="text-sm text-gray-400 hover:text-gray-500" href="#">Contact</Link></li>
+			<li><NavLink to='/contact'  className={({ isActive }) => (isActive ? 'text-blue-500 font-bold ' : 'text-sm text-gray-400 hover:text-gray-500') } >Contact</NavLink></li>
 		</ul>
 
 
@@ -36,10 +36,10 @@ const Navbar = () => {
 		</div>
 
 
-		<div className="lg:hidden drawer ">
+		<div className="lg:hidden ">
 		
 			<div className="flex ml-auto justify-end">
-			<button htmlFor="my-drawer" className="drawer-button flex items-end text-black-600 p-3">
+			<button  onClick={() => setIsMenuOpen(true)} htmlFor="my-drawer" className="drawer-button flex items-end text-black-600 p-3">
 				
 				hellow
 			</button>
@@ -52,12 +52,52 @@ const Navbar = () => {
 	{/* // for mobile */}
 
   
-
-   <div className="drawer-side navbar-menu relative z-50 lg:hidden">
-	<label htmlFor="my-drawer" className="drawer-overlay"></label>
+  {
+	isMenuOpen && <div className=" navbar-menu relative z-50 lg:hidden">
 		<div className="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
-		<MobileMenu></MobileMenu>
+		<nav className="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
+			<div className="flex items-center mb-8">
+				<a className="mr-auto text-3xl font-bold leading-none" href="#">
+					
+				</a>
+				<button  onClick={() => setIsMenuOpen(false)} className="navbar-close">
+					<svg className="h-6 w-6 text-gray-400 cursor-pointer hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+						<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+					</svg>
+				</button>
+			</div>
+			<div>
+				<ul>
+					<li className="mb-1">
+						<a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Home</a>
+					</li>
+					<li className="mb-1">
+						<a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">About Us</a>
+					</li>
+					<li className="mb-1">
+						<a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Services</a>
+					</li>
+					<li className="mb-1">
+						<a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Pricing</a>
+					</li>
+					<li className="mb-1">
+						<a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded" href="#">Contact</a>
+					</li>
+				</ul>
+			</div>
+			<div className="mt-auto">
+				<div className="pt-6">
+					<a className="block px-4 py-3 mb-3 leading-loose text-xs text-center font-semibold bg-gray-50 hover:bg-gray-100 rounded-xl" href="#">Sign in</a>
+					<a className="block px-4 py-3 mb-2 leading-loose text-xs text-center text-white font-semibold bg-blue-600 hover:bg-blue-700  rounded-xl" href="#">Sign Up</a>
+				</div>
+				<p className="my-4 text-xs text-center text-gray-400">
+					<span>Copyright © 2021</span>
+				</p>
+			</div>
+		</nav>
 	</div>
+  }
+   
 
 
 
